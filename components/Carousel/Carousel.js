ae169061d8534326cf67cda34e1cd44d3c240f2f
Michael Harris <mhguitar1997@gmail.com>
2019-05-17 09:23:45 -0700

    Getting stucture in place for stretch goal

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,24 @@
 class Carousel {
+	constructor(carouselElement){
+		this.carouselElement = carouselElement;
+		this.leftButton = carouselElement.querySelector('.left-button');
+		this.rightButton = carouselElement.querySelector('.right-button');
 
+		this.images = document.querySelectorAll('.carousel img');
+		this.leftButton.addEventListener('click', () => this.left());
+		this.rightButton.addEventListener('click', () => this.right());
+	
+	}
+	left(){
+		
+	}
+	right(){
+
+	}
 }
 
-let carousel = document.querySelector();
-
+let carousel = document.querySelector('.carousel');
+new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
