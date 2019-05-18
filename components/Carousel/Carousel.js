d6a7babdddf9b5256acec7a718c754878daa09ff
Michael Harris <mhguitar1997@gmail.com>
2019-05-17 17:12:42 -0700

    Added carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -5,20 +5,28 @@ class Carousel {
 		this.rightButton = carouselElement.querySelector('.right-button');
 
 		this.images = document.querySelectorAll('.carousel img');
-		this.leftButton.addEventListener('click', () => this.left());
-		this.rightButton.addEventListener('click', () => this.right());
-	
-	}
-	left(){
+		[this.rightButton, this.leftButton].forEach(direction => {
+			direction.addEventListener('click', () => this.direction(event));
+		})
 		
+		this.currentIndex = 0;
+		this.lastImage = this.images.length - 1;
+
+		this.images[this.currentIndex].style.display = 'block';
 	}
-	right(){
+	direction(event){
+		this.images.forEach(image => image.style.display = 'none');
+		if(event.target === this.rightButton){
+			this.currentIndex === this.lastImage ? this.currentIndex = 0 : this.currentIndex++;
+		} else {
+			this.currentIndex < 1 ? this.currentIndex = this.lastImage : this.currentIndex--;		}
+		// console.log(this.currentIndex);
+		// console.log(this.lastImage)
+		this.images[this.currentIndex].style.display = 'block';
 
 	}
 }
-
-let carousel = document.querySelector('.carousel');
-new Carousel(carousel);
+new Carousel(document.querySelector('.carousel'););
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
